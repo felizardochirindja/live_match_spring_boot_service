9bebd76cfe76c973755bc9bde67540cdefb72569
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-06-22 15:39:55 +0200

    project setup

---
+++ src/main/java/com/personal/live_match/LiveMatchApplication.java
@@ -0,0 +1,13 @@
+package com.personal.live_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/live_match/LiveMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.live_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
