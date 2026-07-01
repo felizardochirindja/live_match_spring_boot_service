d8bd42accd9f2e2b0a2da00465ee88d5706b2963
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-01 15:18:42 +0200

    feat(service): Replace Objects.requireNonNull with @NonNull annotations
    This commit replaces the explicit null checks using Objects.requireNonNull with Lombok's @NonNull annotations to enforce non-null parameters. This improves code readability and leverages Lombok's compile-time validation to ensure method parameters are not null, reducing boilerplate code. The change also simplifies the method signature by removing redundant null checks.

--- src/main/java/com/personal/live_match/modules/match/business/services/MatchEventLedgerService.java
+++ src/main/java/com/personal/live_match/modules/match/business/services/MatchEventLedgerService.java
@@ -7,10 +7,8 @@ import com.personal.live_match.modules.match.repositories.EventRepository;
 import com.personal.live_match.modules.match.repositories.MatchEventRepository;
 import com.personal.live_match.modules.match.repositories.MatchRepository;
 
-import io.micrometer.common.lang.NonNull;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,20 +22,17 @@ public class MatchEventLedgerService {
     private final EventRepository eventRepository;
 
     public MatchEvent addEventToLedger(
-        Integer matchId,
-        Integer eventId,
+        @NonNull Integer matchId,
+        @NonNull Integer eventId,
         @NonNull String eventMinute,
-        @NonNull int position,
+        int position,
         @NonNull String payload
     ) {
-        Objects.requireNonNull(matchId, "matchId cannot be null");
-        Objects.requireNonNull(eventId, "eventId cannot be null");
-
         Match match = matchRepository.findById(matchId)
             .orElseThrow(() -> new IllegalArgumentException("Match not found with id: " + matchId));
         
         Event event = eventRepository.findById(eventId)
-                .orElseThrow(() -> new IllegalArgumentException("Event not found with id: " + eventId));
+            .orElseThrow(() -> new IllegalArgumentException("Event not found with id: " + eventId));
 
         MatchEvent matchEvent = new MatchEvent(match, event, eventMinute, position, payload);
 
