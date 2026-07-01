0d22b4d4a6c82693744b6f17d8c57930bec5b287
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-01 12:12:31 +0200

    feat(match-event-key): add Lombok annotations for equality and constructor
    Added @AllArgsConstructor and @EqualsAndHashCode to the MatchEventKey class to leverage Lombok for generating constructors and equals/hashCode methods, improving code readability and maintainability.

--- src/main/java/com/personal/live_match/modules/match/business/entities/MatchEventKey.java
+++ src/main/java/com/personal/live_match/modules/match/business/entities/MatchEventKey.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class MatchEventKey implements Serializable{
     @Column(name = "match_id", insertable = false, updatable = false)
     private Integer matchId;
