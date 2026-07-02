7cdfce1d1476378eb21e2e2e362d287173e4f5a8
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-03 00:32:18 +0200

    feat(stream): add match state caching integration
    This commit introduces integration with the MatchStateCacheService to retrieve match states in the LiveStreamController. The controller now fetches match state data from the cache instead of returning a static response. If the match state is found in the cache, it returns a success response with the data; otherwise, it returns a 404 error. This improves performance and provides more accurate match state information.

--- src/main/java/com/personal/live_match/modules/stream/platoforms/api/LiveStreamController.java
+++ src/main/java/com/personal/live_match/modules/stream/platoforms/api/LiveStreamController.java
@@ -3,7 +3,10 @@ package com.personal.live_match.modules.stream.platoforms.api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.personal.live_match.modules.cache.services.MatchStateCacheService;
 import com.personal.live_match.modules.stream.platoforms.api.responses.MatchStateResponse;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/api/matches")
+@RequiredArgsConstructor
 public class LiveStreamController {
+    private final MatchStateCacheService matchStateCacheService;
+
     @GetMapping("/{id}/state")
     public ResponseEntity<MatchStateResponse> getMatchState(@PathVariable Integer id) {
-        var response = new MatchStateResponse("success", "Match state retrieved", null);
+        var matchState = matchStateCacheService.getMatchState(id);
 
-        return ResponseEntity.ok(response);
+        if (matchState.isPresent()) {
+            return ResponseEntity.ok(new MatchStateResponse("success", "Match state retrieved", matchState.get()));
+        }
+
+        return ResponseEntity.status(404).body(new MatchStateResponse("error", "Match state not found", null));
     }
 }
