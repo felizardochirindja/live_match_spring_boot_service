e90d4269d3d36277302e331ff451e75308a74e94
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-02 16:29:03 +0200

    feat: add MatchState class for match state management
    Added a new class `MatchState` to encapsulate the state of a match, including scores, status, and minute. This provides a structured way to handle match data across the application.

---
+++ src/main/java/com/personal/live_match/modules/shared/services/MatchState.java
@@ -0,0 +1,15 @@
+package com.personal.live_match.modules.shared.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatchState {
+    Integer homeScore;
+    Integer awayScore;
+    String status;
+    String minute;
+}
