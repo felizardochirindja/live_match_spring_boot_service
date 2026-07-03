f0d21d40993d2e7eeb25f8e7306f36eb4bf1d112
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-03 16:55:58 +0200

    feat: Update IngestitionController to return ResponseEntity with MatchEventResponse
    The controller now returns a ResponseEntity with a MatchEventResponse instead of a String. This change provides a more structured and informative response to client requests, including a status and message indicating the success of publishing the match event. The method also now uses appropriate HTTP status codes for better API consistency.

--- src/main/java/com/personal/live_match/modules/ingestion/plattform/api/IngestitionController.java
+++ src/main/java/com/personal/live_match/modules/ingestion/plattform/api/IngestitionController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.personal.live_match.modules.events.entities.MatchEventMessage;
 import com.personal.live_match.modules.ingestion.plattform.api.payloads.RegisterEventPayload;
+import com.personal.live_match.modules.ingestion.plattform.api.responses.MatchEventResponse;
+import com.personal.live_match.modules.match.repositories.EventRepository;
+import com.personal.live_match.modules.match.repositories.MatchRepository;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +27,12 @@ public class IngestitionController {
     private final KafkaTemplate<String, MatchEventMessage> kafkaTemplate;
 
     @PostMapping
-    public String registerEvent(@RequestBody RegisterEventPayload payload) {
+    public ResponseEntity<MatchEventResponse> registerEvent(@RequestBody RegisterEventPayload payload) {
         Map<String, Object> map = new HashMap<>();
 
         var eventType =  MatchEventMessage.EventType.valueOf(payload.eventType());
 
-        var event = new MatchEventMessage(
+        var matchEventMessage = new MatchEventMessage(
             payload.eventId(),
             payload.matchId(),
             eventType,
@@ -37,8 +41,10 @@ public class IngestitionController {
             map
         );
 
-        kafkaTemplate.send("match_events", payload.matchId().toString(), event);
+        kafkaTemplate.send("match_events", payload.matchId().toString(), matchEventMessage);
 
-        return "i am here!";
+        var response = new MatchEventResponse("ok", "match event published!");
+
+        return ResponseEntity.status(201).body(response);
     }
 }
