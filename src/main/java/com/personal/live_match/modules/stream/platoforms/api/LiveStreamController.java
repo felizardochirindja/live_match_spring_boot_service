9719851e3d37b5208a72ad1a9f3b51c0e7fb6b39
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-03 02:20:49 +0200

    feat(stream): add event stream endpoint for live match updates
    Added a new endpoint to stream live match updates using Server-Sent Events (SSE). This allows clients to receive real-time updates about match states. The implementation uses the MatchStateSubscriber service to handle event subscriptions and leverages Spring's SseEmitter for streaming.

--- src/main/java/com/personal/live_match/modules/stream/platoforms/api/LiveStreamController.java
+++ src/main/java/com/personal/live_match/modules/stream/platoforms/api/LiveStreamController.java
@@ -4,19 +4,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.personal.live_match.modules.cache.services.MatchStateCacheService;
+import com.personal.live_match.modules.cache.services.MatchStateSubscriber;
 import com.personal.live_match.modules.stream.platoforms.api.responses.MatchStateResponse;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 
 @RestController
 @RequestMapping(value = "/api/matches")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LiveStreamController {
     private final MatchStateCacheService matchStateCacheService;
+    private final MatchStateSubscriber subscriberService;
 
     @GetMapping("/{id}/state")
     public ResponseEntity<MatchStateResponse> getMatchState(@PathVariable Integer id) {
@@ -27,5 +34,10 @@ public class LiveStreamController {
         }
 
         return ResponseEntity.status(404).body(new MatchStateResponse("error", "Match state not found", null));
+    }
+
+    @GetMapping(value = "/{id}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamEvents(@PathVariable Integer id) {
+        return subscriberService.subscribe(id);
     }
 }
