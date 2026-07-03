ae463756cd3f7494bdca1c877446563ec908d17c
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-03 16:28:43 +0200

    feat(events): add Lombok annotations to MatchEventMessage
    Added @AllArgsConstructor and updated imports to use Lombok for better code readability and maintainability. This simplifies the creation of constructor with all fields.

--- src/main/java/com/personal/live_match/modules/events/entities/MatchEventMessage.java
+++ src/main/java/com/personal/live_match/modules/events/entities/MatchEventMessage.java
@@ -2,11 +2,13 @@ package com.personal.live_match.modules.events.entities;
 
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MatchEventMessage {
     private Integer eventId;
     private Integer matchId;
